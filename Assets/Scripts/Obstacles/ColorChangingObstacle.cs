16db0151076f232bd7689980337a966888830450
Nazif Ege Bozoğlu <egebozoglu1@gmail.com>
2022-12-02 12:28:57 +0300

    Update ColorChangingObstacle.cs
    + feat: color changing obstacle functionality, to be continued

--- Assets/Scripts/Obstacles/ColorChangingObstacle.cs
+++ Assets/Scripts/Obstacles/ColorChangingObstacle.cs
@@ -6,16 +6,43 @@ namespace SkinColorRunner.Obstacles
 {
     public class ColorChangingObstacle : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        #region Variables
+        [Header("Materials")]
+        [Space(3)]
+        [SerializeField] private List<Material> materials = new();
 
-        }
+        [Space(5)]
+
+        [Header("Sticks")]
+        [Space(3)]
+        [SerializeField] private List<GameObject> sticks = new();
+
+        // properties
+        private readonly int groupAmount = 3;
+        private int groupIndex = 2;
+        #endregion
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        private IEnumerator ChangeColors()
+        {
+            yield return new WaitForSeconds(1.5f);
+
+            groupIndex -= groupAmount;
+
+            for (int i = 0; i < sticks.Count; i++)
+            {
+                if (groupIndex<0)
+                {
+                    groupIndex = sticks.Count + groupIndex;
+                }
+
+
+            }
         }
     }
 }
