b370166baadc5caef97fae9131a1daf8d4707bed
Nazif Ege Bozoğlu <egebozoglu1@gmail.com>
2022-12-02 17:37:59 +0300

    Update CameraController.cs

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+namespace SkinColorRunner.Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CameraController : MonoBehaviour
     {
-        
-    }
+        public GameObject player;
+        private float offsetZ = 5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void FixedUpdate()
+        {
+            if (player != null)
+            {
+                Vector3 targetPos = new(transform.position.x, transform.position.y, player.transform.position.z - offsetZ);
+                transform.position = Vector3.Lerp(transform.position, targetPos, 0.1f);
+            }
+        }
     }
 }
