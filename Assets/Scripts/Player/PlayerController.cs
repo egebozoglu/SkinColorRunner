619a073e6dc0054595a22b6daa7a8bc4e410c14a
Nazif Ege Bozoğlu <egebozoglu1@gmail.com>
2022-12-02 19:22:09 +0300

    Update PlayerController.cs
    + fix: minor bugs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,7 +7,8 @@ namespace SkinColorRunner.Player
     public class PlayerController : MonoBehaviour
     {
         #region Variables
-        [SerializeField] private float speed = 5f;
+        [SerializeField] private DynamicJoystick joystick;
+        [SerializeField] private float movingSpeed = 5f;
         private Rigidbody rb;
         private Animator animator;
         #endregion
@@ -26,7 +27,25 @@ namespace SkinColorRunner.Player
 
         private void Movement()
         {
-            rb.MovePosition(transform.position + (Time.deltaTime * speed * Vector3.forward));
+            Rotation();
+            rb.MovePosition(transform.position + (Time.deltaTime * movingSpeed * Vector3.forward));
+        }
+
+        private void Rotation()
+        {
+            var horizontal = joystick.Horizontal;
+
+            if (horizontal<0)
+            {
+                horizontal = -1;
+            }
+            else if(horizontal>0)
+            {
+                horizontal = 1;
+            }
+
+            Quaternion targetRotation = Quaternion.Euler(0f, 45f * horizontal, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 0.1f);
         }
     }
 }
