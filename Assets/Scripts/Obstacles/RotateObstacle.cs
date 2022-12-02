217823f37e085ac1511689deca8b907c3020299f
Nazif Ege Bozoğlu <egebozoglu1@gmail.com>
2022-12-02 11:42:15 +0300

    Update RotateObstacle.cs
    + refactor: create functionality of rotation

--- Assets/Scripts/Obstacles/RotateObstacle.cs
+++ Assets/Scripts/Obstacles/RotateObstacle.cs
@@ -6,16 +6,21 @@ namespace SkinColorRunner.Obstacles
 {
     public class RotateObstacle : MonoBehaviour
     {
-        // Start is called before the first frame update
+        [SerializeField] private float speed = 60f;
+        [SerializeField] private bool inverse = false;
+
         void Start()
         {
-
+            if (inverse)
+            {
+                speed *= -1;
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            transform.Rotate(new Vector3(0f, Time.deltaTime * speed, 0f));
         }
     }
 }
